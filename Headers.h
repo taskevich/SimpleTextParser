0fabc068e1b868f26f86d3f39297ace8b812313f
taskevich <93867297+taskevich@users.noreply.github.com>
2021-11-19 17:35:16 +0300

    Create Headers.h

---
+++ Headers.h
@@ -0,0 +1,9 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <wininet.h>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+using namespace std;
